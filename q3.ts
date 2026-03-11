4145203738221dbe90a4f96c0e7bd42d005e9c2f
elisheva1885 <elisheva1885@gmail.com>
2026-03-11 13:19:12 +0200

    working on q3

---
+++ q3.ts
@@ -0,0 +1,15 @@
+function maximumWealth(accounts: number[][]): number {
+    let max = 0;
+    let rowSum = 0;
+    for (let index = 0; index < accounts.length; index++) {
+        console.log(accounts[index]);
+        
+    //    rowSum =  accounts[index]?.reduce((sum, num)=>{
+    //         sum+=num;
+    //     },0)
+        
+    }
+    return max;
+};
+
+maximumWealth([[1,5],[7,3],[3,5]])
