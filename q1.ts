5234f466c7643e31c90322092947e9efd9a8affc
elisheva1885 <elisheva1885@gmail.com>
2026-03-11 11:09:07 +0200

    Solution to question 1

--- q1.ts
+++ q1.ts
@@ -1,15 +1,9 @@
 function mostWordsFound(sentences: string[]): number {
-    let sentence_word: string[]  = [];
-    let max_length = 0;
-    sentences.map(sentence=> {
-        console.log(sentence.split(" ")); 
-        sentence_word  = sentence.split(" ")
-        max_length = max_length <sentence_word.length ? sentence_word.length: max_length;
-        
-    })
-    return max_length;
+    return sentences.reduce((max,x)=>{
+                const sentence_words  = x.split(" ").length
+                return  max = max < sentence_words ? sentence_words: max;
+    },0)
     
 };
  const sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
- 
  console.log(mostWordsFound(sentences)) 
