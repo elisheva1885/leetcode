02f1a169f3bfa606a4408206dcc00bfcdafcbb41
elisheva1885 <elisheva1885@gmail.com>
2026-03-14 22:42:54 +0200

    solution without reduce function

--- q1.ts
+++ q1.ts
@@ -1,9 +1,11 @@
 function mostWordsFound(sentences: string[]): number {
-    return sentences.reduce((max,sentence)=>{
-                const sentenceWords  = sentence.split(" ").length
-                return  max = max < sentenceWords ? sentenceWords: max;
-    },0)
-    
+
+    let max = 0;
+    sentences.forEach((sentence) => {
+        const sentenceWords = sentence.split(" ").length
+        max = Math.max(max, sentenceWords)
+    })
+    return max;
 };
- const sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
- console.log(mostWordsFound(sentences)) 
+const sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+console.log(mostWordsFound(sentences)) 
