8a50d7cc70711b55c3bc0800a07911a96681f121
Yuval Saricov <yuvalsw21104@gmail.com>
2026-03-16 13:48:21 +0200

    feat(): return code to previous version

--- q4.ts
+++ q4.ts
@@ -1,9 +1,8 @@
-
 type LetterInfo = {
-  first: number,
-  last: number,
-  middle: Set<string>
-}
+  first: number;
+  last: number;
+  middle: Set<string>;
+};
 
 function countPalindromicSubsequence(s: string): number {
   const map = new Map<string, LetterInfo>();
@@ -11,33 +10,34 @@ function countPalindromicSubsequence(s: string): number {
   for (let index = 0; index < s.length; index++) {
     const c = s.charAt(index);
     if (!map.get(c)) {
-      map.set(c, { first: index, last: index, middle: new Set() })
-    }
-    else {
+      map.set(c, { first: index, last: index, middle: new Set() });
+    } else {
       map.get(c)!.last = index;
     }
   }
   let sum = 0;
   for (let j = 0; j < s.length; j++) {
     const c = s.charAt(j);
-    set.forEach(letter=> {
-      const letterInfo = map.get(letter)
-      if(j> letterInfo!.first && j< letterInfo!.last){
-        letterInfo?.middle.add(c)
+    set.forEach((letter) => {
+      const letterInfo = map.get(letter);
+      if (j > letterInfo!.first && j < letterInfo!.last) {
+        letterInfo?.middle.add(c);
       }
     });
-    let info = map.get(c)!
-    if(j===info.first || j===info.last){
-      j===info.first?set.add(c): set.delete(c);
+    
+    if (j === map.get(c)!.first) {
+      set.add(c);
+    }
+    if (j === map.get(c)!.last) {
+      set.delete(c);
     }
   }
 
-  map.forEach(info => {
+  map.forEach((info) => {
     sum += info.middle.size;
   });
 
   return sum;
-
-};
+}
 
 console.log(countPalindromicSubsequence("bbcbaba"));
